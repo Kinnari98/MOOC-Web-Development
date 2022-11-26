68c11bd53f8f677364c7e59100aa5617fec24a24
kallekinnari <kinnarikalle@gmail.com>
2022-11-26 16:47:35 +0200

    Exercise 1.8 finished

--- Part 1/unicafe/src/App.js
+++ Part 1/unicafe/src/App.js
@@ -4,6 +4,20 @@ const Button = ({ onClick, buttonName }) => (
   <button onClick={onClick}> {buttonName}</button>
 );
 
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>Total {good + neutral + bad}</p>
+      <p>Average {(good - bad) / (good + neutral + bad)}</p>
+      <p>Positive {(good / (good + neutral + bad)) * 100} %</p>
+    </div>
+  );
+};
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0);
@@ -18,14 +32,7 @@ const App = () => {
       <Button onClick={counter(good, setGood)} buttonName="Good" />
       <Button onClick={counter(neutral, setNeutral)} buttonName="Neutral" />
       <Button onClick={counter(bad, setBad)} buttonName="Bad" />
-
-      <h2>Statistics</h2>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>Total {good + neutral + bad}</p>
-      <p>Average {(good - bad) / (good + neutral + bad)}</p>
-      <p>Positive {(good / (good + neutral + bad)) * 100} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
