9100a2340072006e36fea103f05dfe41fcb63397
kallekinnari <kinnarikalle@gmail.com>
2022-11-26 15:48:08 +0200

    Simple feedback app exercise 1.6

--- Part 1/unicafe/src/App.js
+++ Part 1/unicafe/src/App.js
@@ -1,12 +1,25 @@
 import { useState } from "react";
 
+const Button = ({ onClick }) => <button onClick={onClick}></button>;
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <h1>Give feedback to improve our cafe!</h1>
+      <Button onClick={increment(good, setGood)}>Good</Button>
+      <Button onClick={increment(neutral, setNeutral)}>Neutral</Button>
+      <Button onClick={increment(bad, setBad)}>Bad</Button>
+
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+    </div>
+  );
 };
 
 export default App;
