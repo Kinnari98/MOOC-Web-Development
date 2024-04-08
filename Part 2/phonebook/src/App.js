f3edda39680c1cdc2da440066dd3ca626f70d8a7
Kalle <kinnarikalle@gmail.com>
2024-04-08 18:36:53 +0300

    Started section b. 2.6 and 2.7 done

---
+++ Part 2/phonebook/src/App.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    //Tarkistetaan olemassa oleva henkilö, toteutetaan tämä if-lauseella
+
+    const doesNameExist = persons.some((person) => person.name === newName);
+    if (doesNameExist) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      const newPerson = {
+        name: newName,
+      };
+      // Lisää uuden henkilön persons tilaan, jos nimeä ei ole luettelossa
+      setPersons(persons.concat(newPerson));
+    }
+  };
+
+  // Päivittää newName tilan käyttäjän antaman syötteen mukaan
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}{" "}
+    </div>
+  );
+};
+
+export default App;
