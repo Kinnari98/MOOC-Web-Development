88ad634cd1a79fa8ba352ac5f71af5469ca8f6b4
Kalle <kinnarikalle@gmail.com>
2024-04-25 17:46:54 +0300

    3.12 done

---
+++ Part 3/mongo.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const password = process.argv[2];
+
+const url = `mongodb+srv://kinnarikalle:${password}@project1.dofbubw.mongodb.net/?retryWrites=true&w=majority&appName=project1`;
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 3) {
+  console.log("phonebook:");
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
